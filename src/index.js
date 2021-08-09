70989a4ed6e3a776bdde83bfd11fb96e35d783cd
fujiya <frolov604@gmail.com>
2021-08-09 23:50:27 +0200

    feat: add morse decoder solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,22 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+
+  function findLetter(code) {
+    let ltr = code.substring(code.indexOf('1'), 10);
+    return MORSE_TABLE[ltr.replace(/10/g, '.').replace(/11/g, '-')];
+  }
+
+  for (let i = 0; i < expr.length; i += 10) {
+    result += findLetter(expr.substring(i, i + 10));
+  }
+
+  return result;
 }
 
 module.exports = {
